aa51d8f983f51b7a15609eb86f3fa24e78f9dc28
Keerthanadr <105333912+Keerthanadr@users.noreply.github.com>
2022-08-02 10:49:49 +0530

    Add files via upload

---
+++ sign_in_.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class sign_in_ {
+	@Test
+    public void launch() throws InterruptedException
+    {
+    System.setProperty("webdriver.chrome.driver","./soft/chromedriver.exe");
+    WebDriver driver=new ChromeDriver();
+    driver.get("https://mobileworld.azurewebsites.net/");
+    driver.manage().window().maximize();
+    driver.findElement(By.xpath("//button[@class='btn btn-warning my-2 my-sm-0']")).click();
+    driver.findElement(By.xpath("//a[@href='signup.html']")).click();
+    driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("keerthana");
+    driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("gowda");
+    driver.findElement(By.xpath("//input[@placeholder='Enter Email']")).sendKeys("keerthana@gmail.com");
+    driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("keeru@123");
+    driver.findElement(By.xpath("//input[@type='date']")).sendKeys("01-07-2022");
+    driver.findElement(By.xpath("//div[@class='col-md-2']/input[@type='radio']")).click();
+    driver.findElement(By.xpath("//input[@placeholder='91XXXXXXXXXX']")).sendKeys("9071676564");
+    driver.findElement(By.xpath("//textarea[@placeholder='Short Bio']")).sendKeys("hi everyone");
+    driver.findElement(By.xpath("//button[@class='btn btn-info form-control']")).click();
+    Thread.sleep(2000);
+    driver.switchTo().alert().accept();
+    driver.findElement(By.xpath("//button[@type='Submit']")).click();
+    driver.switchTo().alert().accept();
+    Thread.sleep(2000);
+    driver.findElement(By.id("username")).sendKeys("keerthana gowda");
+    driver.findElement(By.id("password")).sendKeys("keeru@123");
+    driver.findElement(By.xpath("//a[@class='btn btn-primary btn-block']")).click();
+    driver.quit();
+    }    
+}
+	
